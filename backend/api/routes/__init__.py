c32ce85b55c0738cd1594108bae9cca57f7581fb
dickyadi <dickyadi44@gmail.com>
2026-01-19 17:01:28 +0700

    first commit

--- backend/api/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from .v1 import router as v1
-
-router = APIRouter(prefix='/api')
-router.include_router(v1)
--- backend/api/routes/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .helloworld import router as hw
-from .chatbot import router as chatbot
-
-router = APIRouter()
-
-router.include_router(hw)
-router.include_router(chatbot)
